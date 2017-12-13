a12f09b89f3aed0e351b292fac04a990358825c8
Devansh Gautam <devanshg27@gmail.com>
2017-12-13 18:05:03 +0530

    Made changes to suffix automaton

--- Templates/strings/suffix-automaton.cpp
+++ Templates/strings/suffix-automaton.cpp
@@ -1,58 +1,61 @@
+// To check substring/LCS, run the string on the automaton. Each path in the automaton is a substring(if it ends in a terminal node, it is a suffix)
+// To find occurences of a string, run it on the automaton, and the number of its occurences would be number of ways to reach a terminal node.
+// Or, we can keep reverse edges of suffix links(all prefixes for that substring), and number of ways to reach a root, would be the answer(can be used to print all answers)
 struct SuffixAutomaton {
-	vector<map<char,int>> edges; // edges[i]  : the labeled edges from node i
-	vector<int> link;            // link[i]   : the parent of i
-	vector<int> length;          // length[i] : the length of the longest string in the ith class
-	int last;                    // the index of the equivalence class of the whole string
+  vector<map<char,int>> edges; // edges[i]  : the labeled edges from node i
+  vector<int> link;            // link[i]   : the parent of i
+  vector<int> length;          // length[i] : the length of the longest string in the ith class
+  int last;                    // the index of the equivalence class of the whole string
 
-	SuffixAutomaton(string s) {
-		// add the initial node
-		edges.push_back(map<char,int>());
-		link.push_back(-1);
-		length.push_back(0);
-		last = 0;
+  SuffixAutomaton(string s) {
+    // add the initial node
+    edges.push_back(map<char,int>());
+    link.push_back(-1);
+    length.push_back(0);
+    last = 0;
 
-		for(int i=0;i<s.size();i++) {
-			// construct r
-			edges.push_back(map<char,int>());
-			length.push_back(i+1);
-			link.push_back(0);
-			int r = edges.size() - 1;
+    for(int i=0;i<s.size();i++) {
+      // construct r
+      edges.push_back(map<char,int>());
+      length.push_back(i+1);
+      link.push_back(0);
+      int r = edges.size() - 1;
 
-			// add edges to r and find p with link to q
-			int p = last;
-			while(p >= 0 && edges[p].find(s[i]) == edges[p].end()) {
-				edges[p][s[i]] = r;
-				p = link[p];
-			}
-			if(p != -1) {
-				int q = edges[p][s[i]];
-				if(length[p] + 1 == length[q]) {
-					// we do not have to split q, just set the correct suffix link
-					link[r] = q;
-				} else {
-					// we have to split, add q'
-					edges.push_back(edges[q]); // copy edges of q
-					length.push_back(length[p] + 1);
-					link.push_back(link[q]); // copy parent of q
-					int qq = edges.size()-1;
-					// add qq as the new parent of q and r
-					link[q] = qq;
-					link[r] = qq;
-					// move short classes pointing to q to point to q'
-					while(p >= 0 && edges[p][s[i]] == q) {
-						edges[p][s[i]] = qq;
-						p = link[p];
-					}
-				}
-			}
-			last = r;
-		}
+      // add edges to r and find p with link to q
+      int p = last;
+      while(p >= 0 && edges[p].find(s[i]) == edges[p].end()) {
+        edges[p][s[i]] = r;
+        p = link[p];
+      }
+      if(p != -1) {
+        int q = edges[p][s[i]];
+        if(length[p] + 1 == length[q]) {
+          // we do not have to split q, just set the correct suffix link
+          link[r] = q;
+        } else {
+          // we have to split, add q'
+          edges.push_back(edges[q]); // copy edges of q
+          length.push_back(length[p] + 1);
+          link.push_back(link[q]); // copy parent of q
+          int qq = edges.size()-1;
+          // add qq as the new parent of q and r
+          link[q] = qq;
+          link[r] = qq;
+          // move short classes pointing to q to point to q'
+          while(p >= 0 && edges[p][s[i]] == q) {
+            edges[p][s[i]] = qq;
+            p = link[p];
+          }
+        }
+      }
+      last = r;
+    }
 
-		vector<int> terminals;
-		int p = last;
-		while(p > 0) {
-			terminals.push_back(p);
-			p = link[p];
-		}
-	}
+    vector<int> terminals;
+    int p = last;
+    while(p > 0) {
+      terminals.push_back(p);
+      p = link[p];
+    }
+  }
 };
