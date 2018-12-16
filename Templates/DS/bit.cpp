2a572caba309dcfc6dc25ab408dc6c36b8c5e374
anurudh <(none)>
2018-12-16 18:53:16 +0530

    compr

--- Templates/DS/bit.cpp
+++ Templates/DS/bit.cpp
@@ -1,14 +1,5 @@
-// update: normal point update
-// query: normal point query
-/// range update range query: 
-void range_update(int val, int a, int b) {
-  update(bit1, val, a);
-  update(bit1, -val, b+1);
-  update(bit2, (a-1)*val, a);
-  update(bit2, -b*val ,b+1);
-  return;
-}
-// now it gives sum from 1-idx. 
-int pre_sum(int idx) {
-  return idx * query(bit1, idx) - query(bit2, idx);
-}
+void range_update(int val, int a, int b) {// range update range query
+  update(bit1, val, a); update(bit1, -val, b+1);
+  update(bit2, (a-1)*val, a); update(bit2, -b*val ,b+1); }
+int pre_sum(int idx) {// now it gives sum from 1-idx. 
+  return idx * query(bit1, idx) - query(bit2, idx); }
