bf840896110680518fc526032d189f458a0fa3e5
anurudh <(none)>
2019-11-19 04:12:00 +0530

    simplify KMP

--- Templates/strings/KMP.cpp
+++ Templates/strings/KMP.cpp
@@ -1,17 +1,13 @@
-const int N=250005;
-int lps[N],ind[N];
-void compute(string p) {//lps represents
-  int i=1,j=0; lps[0]=0;//longest length
-  while(i<sz(p)) {
-    if(p[i]==p[j]) lps[i]=j+1,i++,j++;
-    else { if(j) j=lps[j-1];
-else lps[i++]=0;
-    }}}
-void KMP(string t, string p) {
-  compute(p); if(sz(p)>sz(t)) return; int i=0,j=0;
-  while(i<sz(t)) {//max matching string till i
-    if(t[i]==p[j]) ind[i]=j+1,i++,j++;
-    if(j==sz(p)) j=lps[j-1];
-    else if(i<sz(t) && t[i]!=p[j]) {
-  if(!j) ind[i]=0,i++; else j=lps[j-1];
-}}}
+vector<int> prefix_function(string s) {
+	int n = (int) s.length();
+	vector<int> pi(n);
+	for (int i = 1; i < n; i++) {
+		int j = pi[i-1];
+		while (j > 0 && s[i] != s[j])
+			j = pi[j-1];
+		if (s[i] == s[j])
+			j++;
+		pi[i] = j;
+	}
+	return pi;
+}
