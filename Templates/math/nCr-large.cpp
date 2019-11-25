4de14275763ed14b66cd1e563754d249318c5040
Arjun P <arjunpitchanathan@gmail.com>
2019-11-25 21:26:13 +0100

    [nCr-large] fix compile errors

--- Templates/math/nCr-large.cpp
+++ Templates/math/nCr-large.cpp
@@ -9,6 +9,15 @@ LL invert_mod(LL k,LL m){
     m1=k1;k1=r;neg=!neg;
   }return neg?m-p2:p2;
 }
+// Preconditions:0<=k<=min(n,p-1);p>1 prime
+LL choose_mod_two(LL n,LL k,LL p){
+  n%=p;if(n<k)return 0;
+  if(k==0||k==n)return 1;
+  if(k>n/2)k=n-k;LL num=n,den=1;
+  for(n=n-1;k>1;--n,--k)num=(num*n)%p,den=(den*k)%p;
+  den=invert_mod(den,p);
+  return (num*den)%p;
+}
 // Preconditions:0<=k<=n;p>1 prime
 LL choose_mod_one(LL n,LL k,LL p){
   if(k<p)return choose_mod_two(n,k,p);
@@ -18,23 +27,7 @@ LL choose_mod_one(LL n,LL k,LL p){
   choose*=choose_mod_one(q_n,q_k,p);
   return choose%p;
 }
-// Preconditions:0<=k<=min(n,p-1);p>1 prime
-LL choose_mod_two(LL n,LL k,LL p){
-  n%=p;if(n<k)return 0;
-  if(k==0||k==n)return 1;
-  if(k>n/2)k=n-k;LL num=n,den=1;
-  for(n=n-1;k>1;--n,--k)num=(num*n)%p,den=(den*k)%p;
-  den=invert_mod(den,p);
-  return (num*den)%p;
-  17
-}
 LL fact_exp(LL n, LL p){
   LL ex=0;do{n/=p;ex+=n;
   }while(n>0);return ex;
-}
-//returns nCk % p in O(p).n and k can be large.
-LL choose_mod(LL n, LL k, LL p){
-  if(k<0||n<k)return 0;if(k==0||k==n)return 1;
-  if(fact_exp(n)>fact_exp(k)+fact_exp(n-k))return 0;
-  return choose_mod_one(n,k,p);
 }
