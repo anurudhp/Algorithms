46406e86c986c64cf81b41fcf837eba2a7a3230d
anurudh <(none)>
2019-12-12 07:05:31 +0530

    fix header

--- Templates/0-Misc/defines.cpp
+++ Templates/0-Misc/defines.cpp
@@ -1,1 +1,6 @@
-using LL = long long; using ll = long long;
+using LL = long long; ll = long long;   DBL = double;
+PII = pair<int, int>; VI = vector<int>; VVI = vector<VI>;
+#define FOR(i,a,b) for(int i=a;i<=b;i++)
+#define FORD(i,a,b) for(int i=a;i>=b;i--)
+#define SZ(a) (int)((a).size())
+#define PB push_back #define MP make_pair
