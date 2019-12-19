c6b8829784ea7397441e6be5f708328ab9f7fef8
Arjun P <arjunpitchanathan@gmail.com>
2019-12-19 02:30:03 +0100

    Update Divide-and-Conquer.cpp

--- Templates/DP/Divide-and-Conquer.cpp
+++ Templates/DP/Divide-and-Conquer.cpp
@@ -1,10 +1,16 @@
-// can loop from optl to optr OR l to r
-// CANNOT LOOP FROM L TO OPTL
-void solve(int j, int l, int r, int optl, int optr){
-  if(l > r) return;
-  int mid = (l + r) >> 1; dp[j][mid] = INF;
-  int low = optl, high = min(optr, mid), opt = optl;
-  for(int k=low; k<=high; ++k){
-    if(dp[j][mid] < get(j, k, mid)) dp[j][mid] = get(j, k, mid), opt = k;
-  } solve(j, l, mid - 1, optl, opt); solve(j, mid + 1, r, opt, optr);
-} (< for MAX) and (> for MIN)
+// Applicable when dp[k][i] = min_{j < i}(dp[k - 1][j] + cost(j, i)) and opt_k(i) <= opt_k(i + 1)
+// This holds when cost(a, d) - cost(a, c) >= cost(b, d) - cost(b, c) for a <= b <= c <= d
+ll cost(int i, int j); // cost to go from i to j, 1-indexed. call solve(2) onwards after computing dp[1]
+void solve(int i, int l = 1, int r = n, int optl = 1, int optr = n) {
+	if (l > r || optl > optr) return;
+	int mid = (l + r)/2;
+	pair<ll, int> best = {inf, -1};
+	for (int j = optl; j <= min(mid, optr); j++) {
+    pair<ll,int> cand(dp[i - 1][j] + cost(j, mid), j);
+    if (best.second == -1) best = cand;
+    else best = min(best, cand);
+	}
+  dp[i][mid] = best.first;
+	solve(i, l, mid - 1, optl, best.second);
+	solve(i, mid + 1, r, best.second, optr);
+}
