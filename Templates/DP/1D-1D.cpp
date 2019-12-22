1c5c55ac7a03324d23bf4fd5c9c58dbb658d1441
Arjun P <arjunpitchanathan@gmail.com>
2019-12-23 00:14:58 +0400

    Compress 1d-1d

--- Templates/DP/1D-1D.cpp
+++ Templates/DP/1D-1D.cpp
@@ -1,8 +1,11 @@
 #define until first
 #define opt second
-// Applicable if dp[i] = min_{j>i}(dp[j] + cost(i,j}) s.t. opt[i] <= opt[j] when i <= j (which holds if
-// cost(a, d) - cost(a, c) >= cost(b, d) - cost(b, c) for a <= b <= c <= d (better to expand smaller segment)
-ll cost(int i, int j) { return dp[j] + /*cost to jump from i to j*/; }
+// Applicable if dp[i] = min_{j>i}(dp[j] + cost(i,j}) s.t.
+// opt[i] <= opt[j] when i <= j (which holds if
+// cost(a, d) - cost(a, c) >= cost(b, d) - cost(b, c) for
+// a <= b <= c <= d (better to expand smaller segment)
+ll cost(int i, int j) {
+  return dp[j] + /*cost to jump from i to j*/; }
 void solve() {
   dp[n] = 0;
   vector<pair<int,int>> v;
@@ -13,14 +16,14 @@ void solve() {
       ipos++;
     }
     dp[i] = cost(i, v[ipos].opt);
-    while (v.back().until < i && cost(v.back().until, i) <= cost(v.back().until, v.back().opt)) {
-      v.pop_back();
-    }
+    while (v.back().until < i && cost(v.back().until, i)
+      <= cost(v.back().until, v.back().opt)) { v.pop_back(); }
     int l = 1, r = min(i - 1, v.back().until);
     while (l <= r) {
       int mid = (l + r)/2;
-      if (cost(mid, i) <= cost(mid, v.back().opt)) l = mid + 1;
-      else r = mid - 1;
+      if (cost(mid, i) <= cost(mid, v.back().opt)) {
+        l = mid + 1; }
+      else { r = mid - 1; }
     }
     if (l - 1 >= 1) {
       v.emplace_back(l - 1, i);
