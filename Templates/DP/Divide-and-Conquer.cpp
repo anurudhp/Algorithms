543ef0fa4a6f4588a67b7b686df6eeae0ef7226e
Arjun P <arjunpitchanathan@gmail.com>
2019-12-23 00:02:48 +0400

    Update Divide-and-Conquer.cpp

--- Templates/DP/Divide-and-Conquer.cpp
+++ Templates/DP/Divide-and-Conquer.cpp
@@ -1,9 +1,9 @@
 // Applicable when dp[k][i] = min_{j < i}(dp[k - 1][j] + cost(j, i)) and opt_k(i) <= opt_k(i + 1)
 // This holds when cost(a, d) - cost(a, c) >= cost(b, d) - cost(b, c) for a <= b <= c <= d
 ll cost(int i, int j); // cost to go from i to j, 1-indexed. call solve(2) onwards after computing dp[1]
-void solve(int i, int l = 1, int r = n, int optl = 1, int optr = n) {
+void solve(int i, int l, int r, int optl, int optr) {
   if (l > r || optl > optr) return;
-  mid = (l + r)/2;
+  int mid = (l + r)/2;
   pair<ll, int> best = {inf, -1};
   for (int j = optl; j <= min(mid, optr); j++) {
     pair<ll,int> cand(dp[i - 1][j] + cost(j, mid), j);
