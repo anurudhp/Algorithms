2f646405b9670585d87225276706183e02481bed
anurudhp <anurudhp97@gmail.com>
2019-12-23 17:51:25 +0530

    dinics: compress

--- Templates/0-Misc/defines.cpp
+++ Templates/0-Misc/defines.cpp
@@ -4,5 +4,7 @@ VD = vector<DBL>; VVD = vector<VD>;
 #define FOR(i,a,b) for(int i=a;i<b;i++)
 #define REP(i,n) FOR(i,0,n)
 #define SZ(a) (int)((a).size())
-#define PB push_back #define MP make_pair
+#define EMB emplace_back
+#define PB push_back
+#define MP make_pair
 #define ALL(x) (x).begin(), (x).end()
