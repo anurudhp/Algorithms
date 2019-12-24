27a4b5ef0219700b0c6bf03ca991c0f66ef9f849
anurudhp <anurudhp97@gmail.com>
2019-12-24 22:57:52 +0530

    fix pollard rho

--- Templates/Math/PollardRho.cpp
+++ Templates/Math/PollardRho.cpp
@@ -1,9 +1,9 @@
-//Expected complexity - O(n^1/4 log(n))
-long long mult(long long a, long long b, long long mod) {return (__int128)a * b % mod;}
-long long f(long long x, long long c, long long mod) {return (mult(x, x, mod) + c) % mod;}
-long long rho(long long n, long long x0=2, long long c=1) {
-  long long x = x0, y = x0, g = 1;
+// Expected complexity - O(n^1/4 log(n))
+LL llmult(LL a, LL b, LL mod) {return ((__int128)a * b) % mod;}
+LL f(LL x, LL c, LL mod) {return (llmult(x, x, mod) + c) % mod;}
+LL rho(LL n, LL x0=2, LL c=1) { LL x = x0, y = x0, g = 1;
   while (g == 1) {
     x = f(x, c, n); y = f(y, c, n);
-    y = f(y, c, n); g = gcd(abs(x - y), n);
-}return g;}//if g = n => repeat with different x0/c, else g is the factor
+    y = f(y, c, n); g = __gcd(llabs(x - y), n);
+  } return g;
+}// if g = n => repeat with different x0/c, else g is a factor
