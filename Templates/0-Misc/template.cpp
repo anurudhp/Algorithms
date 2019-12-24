dd0ce2deb7a1f7f723d40ff1e0e673562e97eb6a
anurudhp <anurudhp97@gmail.com>
2019-12-24 14:58:16 +0530

    update template

--- Templates/0-Misc/template.cpp
+++ Templates/0-Misc/template.cpp
@@ -1,8 +1,7 @@
 #define trace(...) {__f(#__VA_ARGS__, __VA_ARGS__);}
-template <typename Arg1>
-void __f(const char* name, Arg1&& arg1) {
-  cerr << name << " = " << arg1 << std::endl; }
-template <typename Arg1, typename... Args>
+template<typename Arg> void __f(const char* name, Arg&& arg) {
+  cerr << name << " = " << arg << std::endl;
+} template <typename Arg1, typename... Args>
 void __f(const char* names, Arg1&& arg1, Args&&... args) {
   const char* comma = strchr(names + 1, ',');
   cerr.write(names, comma - names) << " = " << arg1<<" | ";__f(comma+1, args...); }
@@ -13,12 +12,10 @@ typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_
 #pragma GCC optimize("Ofast")
 optimize("unroll-loops")
 target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
-
-//https://codeforces.com/blog/entry/60737
-//gp_hash_table has far superior performance for insertions/deletions, while cc has marginally better performance for reads/writes
-gp_hash_table<int, int> table;
-const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
-struct chash {
-    int operator()(int x) const { return x ^ RANDOM; }
-};
-gp_hash_table<key, int, chash> table;
+/*** RNGS ***/
+SEED=chrono::steady_clock::now().time_since_epoch().count();
+// or high_resolution_clock
+mt19937 rng(SEED); /*or*/ random_device rd; mt19937 rng(rd());
+uniform_int_distribution<> dis(MIN, MAX);// usage: dis(rng)
+struct chash {int operator()(int x) const {return x^SEED;}};
+gp_hash_table<int,int,chash>m;//use cc if very less updates
