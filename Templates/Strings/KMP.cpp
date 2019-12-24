5fc75207f7615a82a681033b4755b517a940d3f7
devanshg27 <devanshg27@gmail.com>
2019-12-24 15:44:53 +0530

    Update KMP.cpp

--- Templates/Strings/KMP.cpp
+++ Templates/Strings/KMP.cpp
@@ -5,3 +5,9 @@ vector<int> prefix_function(string s) {
     if (s[i] == s[j]) j++;
     pi[i] = j; }
   return pi; }
+void compute_automaton(string s, vector<vector<int>>& aut) {
+  s += '#';vector<int> pi = prefix_function(s);
+  aut.assign(SZ(s), vector<int>(26));
+  FOR(i,0,SZ(s)) {FOR(c,0,26) {
+    if (i > 0 && 'a' + c != s[i]) aut[i][c] = aut[pi[i-1]][c];
+    else aut[i][c] = i + ('a' + c == s[i]);}}}
