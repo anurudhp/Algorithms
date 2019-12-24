70b2f8a015a498d01c53227dec7731e41128e4be
anurudhp <anurudhp97@gmail.com>
2019-12-24 18:58:50 +0530

    fix bugs in FWHT

--- Templates/Math/Convolutions/WalshHadamard.cpp
+++ Templates/Math/Convolutions/WalshHadamard.cpp
@@ -1,6 +1,6 @@
 void WalshHadamard(poly& P, bool invert) {
-  for (len = 1; 2 * len <= SZ(P); len <<= 1) {
-    for (i = 0; i < SZ(P); i += 2 * len) {
+  for (int len = 1; 2 * len <= SZ(P); len <<= 1) {
+    for (int i = 0; i < SZ(P); i += 2 * len) {
       FOR(j, 0, len) {
         auto u = P[i+j], v = P[i+len+j];
         P[i+j] = u + v, P[i+len+j] = u - v; // XOR [inv:same]
