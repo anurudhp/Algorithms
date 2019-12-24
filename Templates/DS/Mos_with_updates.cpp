03bf0951cb21b22c747d39488cdb78318e6f7ea7
anurudhp <anurudhp97@gmail.com>
2019-12-24 20:59:39 +0530

    compress MO

--- Templates/DS/Mos_with_updates.cpp
+++ Templates/DS/Mos_with_updates.cpp
@@ -1,14 +1,12 @@
 //query = {l, r, id, t} sort by {l/block, r/block, t}
 //update = {pos, pre, now} - point update at position
 // complexity = O(n^5/3) : set block = n^2/3
-for(int i = 0; i < (int)q.size(); i++) {
+REP(i, SZ(q)) {
 	while(t < q[i].t) ++t, apply(u[t].pos, u[t].now);
 	while(t > q[i].t) apply(u[t].pos, u[t].pre), --t;
-
 	while(r < q[i].r) add(++r);
 	while(l > q[i].l) add(--l);
 	while(r > q[i].r) remove(r--);
 	while(l < q[i].l) remove(l++);
-
 	ans[q[i].id] = ds.get();
 }
