77181f796b7dd239078588abc0524b631f20ac2d
anurudhp <anurudhp97@gmail.com>
2019-12-24 15:43:10 +0530

    remove bit,persistent

--- Templates/DS/bit.cpp
+++
@@ -1,5 +0,0 @@
-void range_update(int val, int a, int b) {// range update range query
-  update(bit1, val, a); update(bit1, -val, b+1);
-  update(bit2, (a-1)*val, a); update(bit2, -b*val ,b+1); }
-int pre_sum(int idx) {// now it gives sum from 1-idx. 
-  return idx * query(bit1, idx) - query(bit2, idx); }
