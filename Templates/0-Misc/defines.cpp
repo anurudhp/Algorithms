c4b5b4e06b62f04030b7349bdd772329e7ae7bf5
anurudhp <anurudhp97@gmail.com>
2019-12-24 21:07:38 +0530

    update template

--- Templates/0-Misc/defines.cpp
+++ Templates/0-Misc/defines.cpp
@@ -1,9 +1,9 @@
 using LL = long long; ll = long long;   DBL = double;
 PII = pair<int, int>; VI = vector<int>; VVI = vector<VI>;
 VD = vector<DBL>; VVD = vector<VD>;
-#define FOR(i,a,b) for(int i=a;i<b;i++)
+#define FOR(i,a,b) for(int i = a; i < b; i++)
 #define REP(i,n) FOR(i,0,n)
-#define SZ(a) (int)((a).size())
+#define SZ(a) ((int)((a).size()))
 #define EB emplace_back
 #define PB push_back #define MP make_pair
 #define ALL(x) (x).begin(), (x).end()
