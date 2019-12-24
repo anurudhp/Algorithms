993273033842937b523897be3d89847325ad96b4
anurudhp <anurudhp97@gmail.com>
2019-12-24 13:37:17 +0530

    fix spacing

--- Templates/Strings/Z-Algo.cpp
+++ Templates/Strings/Z-Algo.cpp
@@ -1,8 +1,7 @@
-vector<int> computeZ(string s) {
-  vector<int> z(s.size());
-	int l = -1, r = -1; z[0] = SZ(s);
-	FOR(i,1,SZ(s)) { z[i]=0;
-		if (r > i) z[i] = min(z[i-l],r-i+1);
-		while (s[i+z[i]] == s[z[i]]) ++z[i];
-		if (i+z[i]-1 > r) r = i+z[i]-1, l = i; } 
-  return z; }
+VI computeZ(string s) { VI z(s.size());
+  int l = -1, r = -1; z[0] = SZ(s);
+  FOR(i,1,SZ(s)) { z[i]=0;
+    if (r > i) z[i] = min(z[i-l],r-i+1);
+    while (s[i+z[i]] == s[z[i]]) ++z[i];
+    if (i+z[i]-1 > r) r = i+z[i]-1, l = i;
+  } return z; }
