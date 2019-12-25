3b2957de4cee3c7a1c10d2c9fa0d90a3f389ab68
devanshg27 <devanshg27@gmail.com>
2019-12-25 13:54:06 +0530

    Update 2SAT.cpp

--- Templates/Graphs/2SAT.cpp
+++ Templates/Graphs/2SAT.cpp
@@ -2,6 +2,6 @@ VVI adj; // indexing: var(i) = 2i, not(i) = 2i+1
 void implies(int u, int v) { adj[u].PB(v); }
 void add_or (int x, int y) { implies(1^x, y); implies(1^y, x); }
 void add_xor(int x, int y) { add_or(x, y); add_or(1^x, 1^y); }
-void add_eq (int x, int y) { implies(x, y); implies(y, x); }
+void add_eq (int x, int y) { add_or(x, 1^y); add_or(y, 1^x); }
 void add_true(int x) { implies(1^x, x); }
 // Run SCC, assign true greedily in reverse toposort order
