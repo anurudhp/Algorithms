a86d3182f86cf88a1227af69c2ea91278783578b
devanshg27 <devanshg27@gmail.com>
2019-12-25 13:50:36 +0530

    Update PollardRho.cpp

--- Templates/Math/PollardRho.cpp
+++ Templates/Math/PollardRho.cpp
@@ -2,7 +2,7 @@
 LL llmult(LL a, LL b, LL mod) {return ((__int128)a * b) % mod;}
 LL f(LL x, LL c, LL mod) {return (llmult(x, x, mod) + c) % mod;}
 LL rho(LL n, LL x0=2, LL c=1) { LL x = x0, y = x0, g = 1;
-  while (g == 1) {
+  if(n%2==0)return 2; while (g == 1) {
     x = f(x, c, n); y = f(y, c, n);
     y = f(y, c, n); g = __gcd(llabs(x - y), n);
   } return g;
