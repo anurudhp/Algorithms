e03321778f546052c874554a7a6dd2e9f5c9de28
anurudhp <anurudhp97@gmail.com>
2019-12-29 15:12:07 +0530

    add keybindings

--- Templates/Flows/#Dinics.cpp
+++ Templates/Flows/#Dinics.cpp
@@ -6,33 +6,33 @@ struct Dinic { using FT = int;
   int n, m = 0, src, dest; FT LIM = 1;
   vector<Edge> edges; vector<VI> adj; VI level, ptr; 
   Dinic(int n) : n(n), adj(n), level(n), ptr(n) {}
-  void add_edge(int v, int u, FT cap) {
-    edges.EB(v, u, cap); adj[v].PB(m); m++;
-    edges.EB(u, v, 0);   adj[u].PB(m); m++;
+  void add_edge(int u, int v, FT cap) {
+    edges.EB(u, v, cap); adj[u].PB(m); m++;
+    edges.EB(v, u, 0);   adj[v].PB(m); m++;
   }
   bool bfs() { fill(ALL(level), -1); level[src] = 0; 
     VI q(n+10); int qh = 0, qt = -1; q[++qt] = src;
     while (qh <= qt) { // QUEUE be careful
-      int v = q[qh++]; // QUEUE be careful
-      for (int id : adj[v]) {
+      int u = q[qh++]; // QUEUE be careful
+      for (int id : adj[u]) { int v = edges[id].to;
         if (edges[id].cap - edges[id].flow < LIM) continue;
-        if (level[edges[id].to] != -1) continue;
-        level[edges[id].to] = level[v] + 1;
-        q[++qt] = edges[id].to; // QUEUE be careful
+        if (level[v] != -1) continue;
+        level[v] = level[u] + 1;
+        q[++qt] = v; // QUEUE be careful
       }
     } return level[dest] != -1;
   }
-  FT dfs(int v, FT pushed) {
+  FT dfs(int u, FT pushed) {
     if (pushed == 0) return 0;
-    if (v == dest) return pushed;
-    for (int& cid = ptr[v]; cid < SZ(adj[v]); cid++) {
-      int id = adj[v][cid]; int u = edges[id].to;
-      if (level[v] + 1 != level[u]) continue;
-      FT tr = dfs(u, min(pushed, edges[id].cap - edges[id].flow));
+    if (u == dest) return pushed;
+    for (int& cid = ptr[u]; cid < SZ(adj[u]); cid++) {
+      int id = adj[u][cid]; int v = edges[id].to;
+      if (level[u] + 1 != level[v]) continue;
+      FT tr = dfs(v, min(pushed, edges[id].cap - edges[id].flow));
       if (tr > 0) {
         edges[id].flow += tr; edges[id ^ 1].flow -= tr;
         return tr;
-    }} return 0;
+    } } return 0;
   }
   FT max_flow(int s, int t) { src = s, dest = t; FT f = 0;
     // for (LIM = INF >> 1; LIM > 0; LIM >>= 1) // SCALING
