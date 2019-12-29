d9e85978e45c1b4eb0cc892d99d76a30a15d5b65
anurudhp <anurudhp97@gmail.com>
2019-12-29 14:51:51 +0530

    geometry: use explicit constructor

--- Templates/Geometry/2DGeometry.cpp
+++ Templates/Geometry/2DGeometry.cpp
@@ -1,31 +1,31 @@
-#define Vec Vect<T>
+#define vec Vect<T>
 template<typename T> struct Vect { T x, y;
-  Vec(): x(0), y(0) {}
-  Vec(T a, T b): x(a), y(b) {};
+  vec(): x(0), y(0) {}
+  vec(T a, T b): x(a), y(b) {};
   template<class D>
-  Vec(const Vect<D>& o): x(o.x), y(o.y) {};
-  Vec operator+(const Vec& o) const {return Vec(x+o.x, y+o.y);}
-  Vec operator-(const Vec& o) const {return Vec(x-o.x, y-o.y);}
-  Vec operator*(T s) const { return Vec(x*s, y*s); }
-  Vec operator/(T s) const { return Vec(x/s, y/s); }
-  T dot(const Vec& o) const { return x * o.x + y * o.y; }
-  T cross(const Vec& o) const { return x * o.y - y * o.x; }
+  explicit vec(const Vect<D>& v): x(v.x), y(v.y) {};
+  vec operator+(const vec& v) const {return vec(x+v.x, y+v.y);}
+  vec operator-(const vec& v) const {return vec(x-v.x, y-v.y);}
+  vec operator*(T s) const { return vec(x*s, y*s); }
+  vec operator/(T s) const { return vec(x/s, y/s); }
+  T dot(const vec& v) const { return x * v.x + y * v.y; }
+  T cross(const vec& v) const { return x * v.y - y * v.x; }
   T norm() const { return hypot(x, y); }
-  Vec unit() const { return *this / norm(); }
-  Vec rotCCW(T ang) const {
-    return Vec(x*cos(ang) - y*sin(ang), x*sin(ang) + y*cos(ang));
+  vec unit() const { return *this / norm(); }
+  vec rotCCW(T ang) const {
+    return vec(x*cos(ang) - y*sin(ang), x*sin(ang) + y*cos(ang));
   }
 };
 template<typename T> struct Line {
-  Vec a, v; // l(r) = a + rv
-  Line(const Vec& p, const Vec& q) { // through p,q
+  vec a, v; // l(r) = a + rv
+  Line(const vec& p, const vec& q) { // through p,q
     a = p; v = q - p;
-  } Line perpendicular(const Vec& p) const { // perp through p
+  } Line perpendicular(const vec& p) const { // perp through p
     return Line(p, p + v.rotCCW(PI/2));
-  } bool intersect(const Line& o, Vec& p) const {
+  } bool intersect(const Line& o, vec& p) const {
     if (isZero(v.cross(o.v))) return false;
     T alpha = ((o.a - a) / (v.cross(o.v))).cross(o.v);
     p = a + v * alpha; return true;
   }
 };
-#undef Vec
+#undef vec
