ebbdfd9af214f7b1a3984fcc30563b7f084a19aa
anurudhp <anurudhp97@gmail.com>
2020-01-10 02:13:03 +0530

    mobius

--- Templates/Math/Convolutions/Mobius.cpp
+++ Templates/Math/Convolutions/Mobius.cpp
@@ -1,21 +1,14 @@
-VI Mobius(int N) { // return mu(n) for n in [1,N]
-  VI mu(N + 1, 0); mu[1] = 2;
-  FOR(i, 2, N + 1) if (mu[i] == 0) {
-    mu[i] = 1; LL ii = i*i;
-    if (ii <= N) {
-      for (int j = ii; j <= N; j += ii) {
-        mu[j] = -1;
-      }
-    }
-    for (int j = i << 1; j <= N; j += i) {
-      if (mu[j] != -1) ++mu[j];
-    }
-  }
-  FOR(i, 1, N + 1) {
-    mu[i] = (mu[i] == -1) ? 0 : (mu[i] & 1) ? -1 : 1;
+VI mobius(int N) { VI mu(N + 1, 1);
+  vector<bool> ispr(N + 1, 1);
+  for(int i=2; i<=N; ++i) {
+    if(!ispr[i]) continue;
+    for(int j=i; j<=N; j+=i) { ispr[j] = 0; mu[j] *= -1; }
+    if (i*1ll*i > N) continue;
+    for(int j = i*i, ii = i*i; j<=N; j+=ii) mu[j] = 0;
   }
   return mu;
-} VI DirichletConvolution(const VI &A, const VI &B, int N) {
+}
+VI DirichletConvolution(const VI &A, const VI &B, int N) {
   VI ans(N+1,0); // H(n) = sum{x,y:x*y=n} A[x]*B[y] for [1,N]
   FOR(i,1,N+1) for(int j=i;j<=N;j+=i) ans[j] += A[i]*B[j/i];
   return ans;
