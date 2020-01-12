8a3936adad7cd40f39237a3f1bbab198c11ba675
devanshg27 <devanshg27@gmail.com>
2020-01-12 23:56:47 +0530

    Update #SuffixArray.cpp

--- Templates/Strings/#SuffixArray.cpp
+++ Templates/Strings/#SuffixArray.cpp
@@ -1,56 +1,53 @@
-//LCP[0][i]= len(LCP) of SA[i] & SA[i+1](sorted suffixes).
-//RA[i][j] = Rank of suffix S[j...j+2^i]
-//SA[i] = i’th Lexicographically smallest suffix’s index.
-int RA[LOGN][N],SA[N],tempSA[N],cnt[N],msb[N];
-int LCP[LOGN][N],dollar[N];
-void countingSort(int l,int k,int n) { SET(cnt, 0);
-  REP(i,n) { int idx=(i+k<n ? RA[l][i+k] : 0); cnt[idx]++; }
-  int maxi = max(300,n), sum = 0;
-  REP(i,maxi) { int t=cnt[i]; cnt[i]=sum; sum+=t; }
-  REP(i,n){ int idx = SA[i] + k < n ? RA[l][SA[i]+k] : 0;
-    tempSA[cnt[idx]++]=SA[i];
-  } REP(i,n)SA[i]=tempSA[i];
+const int MAXN = 100010, LOGN = 18;
+int blk[MAXN], sa[MAXN], cnt[MAXN], b[MAXN], tmp[MAXN], fast[MAXN];
+
+void suffix_array(const string &s){
+  int n = SZ(s);
+  REP(i, n) sa[i] = n-i-1;
+  stable_sort(sa, sa+n,[&](int i, int j){return s[i]<s[j];});
+  REP(i, n) blk[i]=s[i];
+  for(int len=1;len<n;len*=2){
+    REP(i, n) b[i]=blk[i],tmp[i]=sa[i]-len,cnt[i]=i;
+    REP(i,n)
+      blk[sa[i]]=i&&b[sa[i-1]]==b[sa[i]]&&sa[i-1]+len<n&&b[sa[i-1]+len/2]==b[sa[i]+len/2]?blk[sa[i-1]]:i;
+    REP(i,n) fast[i]=(tmp[i]>=0?blk[tmp[i]]:-1);
+    REP(i,n) if(~fast[i]) sa[cnt[fast[i]]++]=tmp[i];
+  }
 }
-void build_SA(string &s) { const int n = SZ(s);
-  REP(i,n) RA[0][i]=s[i], SA[i]=i;
-  REP(i, LOGN-1) {
-    int k = (1<<i); if (k >= n) break;
-    countingSort(i,k,n); countingSort(i,0,n);
-    int rank = 0; RA[i+1][SA[0]] = rank;
-    FOR(j,1,n) {
-      if (RA[i][SA[j]]  == RA[i][SA[j-1]]
-      && RA[i][SA[j]+k] == RA[i][SA[j-1]+k]) {
-        RA[i+1][SA[j]]=rank;
-      } else { RA[i+1][SA[j]]=++rank; }
-} } }
+vector<int> kasai(string &s){
+  int n = SZ(s), k = 0;
+  VI lcp(n),inv(n);
+  REP(i,n) inv[sa[i]] = i;
+  REP(i,n){
+    if(k<0) k = 0;
+    if(inv[i]==n-1){ k=0; continue; }
+    for(int j=sa[inv[i]+1];i+k<n&&j+k<n&&s[i+k]==s[j+k];++k);
+    lcp[inv[i]] = k--;
+  }
+  return lcp;
+}
+int LCP[LOGN][MAXN], msb[MAXN], dollar[MAXN];
 void build_msb(){ int mx = -1;
-  REP(i,N) { if (i >= (1 << (mx+1))) mx++; msb[i]=mx; }
+  REP(i,MAXN) { if (i >= (1 << (mx+1))) mx++; msb[i]=mx; }
 }
 void build_LCP(string& s){ const int n = SZ(s);
+  auto lcp = kasai(s);
   REP(i,n-1) {//Build the LCP array in O(NlogN)
-    int x = SA[i], y = SA[i+1], ret = 0;
-    for(int k = LOGN-1; k>=0 && x<n && y<n; k--) {
-      if((1<<k) >= n) continue;
-      if(RA[k][x]==RA[k][y]) x += 1<<k, y += 1<<k, ret += 1<<k;
-    } if(ret >= dollar[SA[i]]-SA[i]) ret=dollar[SA[i]]-SA[i];
+    int ret = lcp[i];
+    if(ret >= dollar[sa[i]]-sa[i]) ret=dollar[sa[i]]-sa[i];
     LCP[0][i] = ret; //LCP[i] shouldnt exceed dollar[SA[i]]
   } //dollar[i] : index of dollar to the right of i.
-  LCP[0][n-1]=10*N;
+  LCP[0][n-1]=10*MAXN;
   FOR(i,1,LOGN) {//O(1) RMQ structure in O(NlogN)
     int add = (1<<(i-1));if(add>=n)break;//small optimization
     REP(j,n) { if(j+add<n) {
         LCP[i][j] = min(LCP[i-1][j],LCP[i-1][j+add]);
       } else { LCP[i][j] = LCP[i-1][j]; }
 } } }
-int lcp(int x,int y) { if(x==y) return dollar[SA[x]]-SA[x];
+
+int lcp(int x,int y) { if(x==y) return dollar[sa[x]]-sa[x];
   //O(1) LCP.x & y are indexes of the suffix in SA!
   if(x>y) {swap(x,y);} y--;
   int idx=msb[y-x+1],sub=(1<<idx);
   return min(LCP[idx][x],LCP[idx][y-sub+1]);
-} 
-bool equal(int i,int j,int p,int q){
-  if(j - i != q - p) return false;
-  int idx = msb[j-i+1], sub = (1<<idx);
-  return RA[idx][i] == RA[idx][p] && 
-  RA[idx][j-sub+1] == RA[idx][q-sub+1];
-} //Note : Do not forget to add a terminating ’$’
+}
