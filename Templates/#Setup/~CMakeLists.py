3f5ef36ecf0871daf6a45c23ade59cfd84e99428
anurudhp <anurudhp97@gmail.com>
2020-03-07 04:50:58 +0530

    cmakelists

--- Templates/#Setup/~CMakeLists.py
+++ Templates/#Setup/~CMakeLists.py
@@ -1,7 +1,7 @@
 cmake_minimum_required(VERSION 3.15)
 project(contest)
-set(CMAKE_CXX_STANDARD 14)
-set(CMAKE_CXX_FLAGS, "-std=c++14 -Wall -pedantic -Wconversion \
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_FLAGS, "-std=c++17 -Wall -pedantic -Wconversion \
   -Wshadow -Wfloat-equal")
 if (DEBUG_EXEC) # set in 'Debug'
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O0 -g -fsanitize=address -fsanitize=undefined")
@@ -12,7 +12,7 @@ if (LOCAL_EXEC) # set in 'Debug' and 'Run'
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DLOCAL_EXEC")
 endif()
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ..) # folder with source
-set(srcs A B C) # adjust filenames
+set(srcs a b c d e f g h i j k l m) # adjust names as necessary
 foreach(F IN LISTS srcs)
-  add_executable(${F}.out ${F}.cpp)
+    add_executable(${F}.out ${F}.cpp)
 endforeach()
